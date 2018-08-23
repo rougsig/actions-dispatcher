414eba30e478fa09af66d9d8af5b969acd1ba043
st <rougsig@gmail.com>
2018-08-23 23:47:39 +0200

    Add processor Logger

--- processor/src/main/kotlin/Processor.kt
+++ processor/src/main/kotlin/Processor.kt
@@ -5,20 +5,33 @@ import com.squareup.kotlinpoet.*
 import ru.rougsig.actionsdispatcher.annotations.ActionDispatcher
 import java.io.File
 import java.lang.StringBuilder
-import javax.annotation.processing.AbstractProcessor
+import javax.annotation.processing.*
 import javax.annotation.processing.Processor
-import javax.annotation.processing.RoundEnvironment
 import javax.lang.model.SourceVersion
 import javax.lang.model.element.TypeElement
+import javax.lang.model.util.Types
 
 @AutoService(Processor::class)
 class Processor : AbstractProcessor() {
+  private lateinit var logger: Logger
+  private lateinit var typeUtils: Types
+  private lateinit var filer: Filer
+
   companion object {
     const val KAPT_KOTLIN_GENERATED_OPTION_NAME = "kapt.kotlin.generated"
   }
 
-  override fun getSupportedAnnotationTypes(): MutableSet<String> {
-    return mutableSetOf(ActionDispatcher::class.java.name)
+  @Synchronized
+  override fun init(processingEnv: ProcessingEnvironment) {
+    super.init(processingEnv)
+
+    logger = Logger(processingEnv.messager)
+    typeUtils = processingEnv.typeUtils
+    filer = processingEnv.filer
+  }
+
+  override fun getSupportedAnnotationTypes(): Set<String> {
+    return setOf(ActionDispatcher::class.java.name)
   }
 
   override fun getSupportedSourceVersion(): SourceVersion {
