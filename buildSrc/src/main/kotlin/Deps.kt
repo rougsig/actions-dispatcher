c93eef20fe4d8efb99cd99380434ee8c648e8665
rougsig <rougsig@gmail.com>
2020-08-25 21:12:24 +0200

    Update all dependencies to latest version

---
+++ buildSrc/src/main/kotlin/Deps.kt
@@ -0,0 +1,12 @@
+object Deps {
+  private const val KOTLIN_VERSION = "1.4.0"
+  const val KOTLIN_GRADLE = "org.jetbrains.kotlin:kotlin-gradle-plugin:$KOTLIN_VERSION"
+
+  const val KOTLIN_STD = "org.jetbrains.kotlin:kotlin-stdlib-jdk8:$KOTLIN_VERSION"
+  const val KOTLIN_REFLECT = "org.jetbrains.kotlin:kotlin-reflect:$KOTLIN_VERSION"
+  const val KOTLINPOET = "com.squareup:kotlinpoet:1.6.0"
+  const val KOTLIN_METADATA = "me.eugeniomarletti.kotlin.metadata:kotlin-metadata:1.4.0"
+
+  const val JUNIT = "junit:junit:4.13"
+  const val COMPILE_TESTING = "com.google.testing.compile:compile-testing:0.18"
+}
