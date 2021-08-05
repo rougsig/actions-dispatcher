161e77bf86847bff45bac3185fb02dfa81584e72
Dmitry Suzdalev <dz@kode.ru>
2021-08-05 13:08:41 +0200

    Fix compilation error for projects on kotlin 1.5.x
    Update versions of kotlin and kotlinpoet is all that is needed.

--- buildSrc/src/main/kotlin/Deps.kt
+++ buildSrc/src/main/kotlin/Deps.kt
@@ -1,11 +1,11 @@
 object Deps {
-  private const val KOTLIN_VERSION = "1.4.0"
+  private const val KOTLIN_VERSION = "1.5.21"
   const val KOTLIN_GRADLE = "org.jetbrains.kotlin:kotlin-gradle-plugin:$KOTLIN_VERSION"
 
   const val KOTLIN_STD = "org.jetbrains.kotlin:kotlin-stdlib-jdk8:$KOTLIN_VERSION"
   const val KOTLIN_REFLECT = "org.jetbrains.kotlin:kotlin-reflect:$KOTLIN_VERSION"
 
-  private const val KOTLINPOET_VERSION = "1.6.0"
+  private const val KOTLINPOET_VERSION = "1.9.0"
   const val KOTLINPOET = "com.squareup:kotlinpoet:$KOTLINPOET_VERSION"
   const val KOTLINPOET_METADATA = "com.squareup:kotlinpoet-metadata:$KOTLINPOET_VERSION"
 
